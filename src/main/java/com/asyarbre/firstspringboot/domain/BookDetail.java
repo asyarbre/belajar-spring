1e096215ff6d9c33feafb361c75d8c8f4410a1e6
Asya'ri <asyari818@gmail.com>
2025-07-25 20:42:47 +0700

    feat: create book detail entity

--- src/main/java/com/asyarbre/firstspringboot/domain/BookDetail.java
+++ src/main/java/com/asyarbre/firstspringboot/domain/BookDetail.java
@@ -1,4 +1,25 @@
 package com.asyarbre.firstspringboot.domain;
 
-public class BookDetail {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "book_detail")
+public class BookDetail implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "settings")
+    private String settings;
+
+    @Column(name = "thumbnail")
+    private String thumbnail;
+
+    @OneToOne
+    @JoinColumn(name = "book_id", nullable = true)
+    private Book book;
 }
