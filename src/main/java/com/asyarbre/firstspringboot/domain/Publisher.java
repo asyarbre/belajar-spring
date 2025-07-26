94e718a61fd98f42963acf0215b333495f1ddf46
Asya'ri <asyari818@gmail.com>
2025-07-26 08:16:17 +0700

    feat: create publisher entity

---
+++ src/main/java/com/asyarbre/firstspringboot/domain/Publisher.java
@@ -0,0 +1,29 @@
+package com.asyarbre.firstspringboot.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "publisher")
+public class Publisher implements Serializable {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "publisher_generator")
+    @SequenceGenerator(name = "publisher_generator", sequenceName = "publisher_id_seq")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "company_name", nullable = false)
+    private String companyName;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToMany(mappedBy = "publisher")
+    private List<Book> books;
+}
